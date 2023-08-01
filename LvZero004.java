f29272ebaf8002c4479040b30078c96a450255e0
dls0430 <dls0430@naver.com>
2023-08-01 22:48:21 +0900

    [23.08.01.Tue]-Lv0/Java/대소문자 바꿔서 출력하기

---
+++ LvZero004.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LvZero004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String ans = "";
+
+        for (char x : a.toCharArray()) {
+            if (Character.isUpperCase(x)) {
+                ans += Character.toLowerCase(x);
+            } else {
+                ans += Character.toUpperCase(x);
+            }
+        }
+        System.out.println(ans);
+    }
+}
