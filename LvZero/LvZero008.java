06404abb17508db8c393d2f72d3b2eb285f74824
dls0430 <dls0430@naver.com>
2023-08-02 14:07:48 +0900

    [23.08.02.Wed]-Pro/Lv0/Java/문자열 돌리기

---
+++ LvZero/LvZero008.java
@@ -0,0 +1,14 @@
+package LvZero;
+
+import java.util.Scanner;
+
+public class LvZero008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        for(char x : a.toCharArray()){
+            System.out.println(x);
+        }
+    }
+}
