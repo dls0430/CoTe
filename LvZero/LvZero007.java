7675c9bad8b473c5897dbbe5a69791ca9fcbcb04
dls0430 <dls0430@naver.com>
2023-08-02 11:30:30 +0900

    [23.08.02.Wed]-Pro/Lv0/Java/문자열 붙여서 출력하기

---
+++ LvZero/LvZero007.java
@@ -0,0 +1,13 @@
+package LvZero;
+
+import java.util.Scanner;
+
+public class LvZero007 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a + b);
+    }
+}
