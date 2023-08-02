6848b24c0b692eb1f1b0e7331c6988ffd1c65a4b
dls0430 <dls0430@naver.com>
2023-08-02 11:26:51 +0900

    [23.08.02.Wed]-Pro/Lv0/Java/덧셈식 출력하기

---
+++ LvZero/LvZero006.java
@@ -0,0 +1,14 @@
+package LvZero;
+
+import java.util.Scanner;
+
+public class LvZero006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+
+}
