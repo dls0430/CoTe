c39d7e183b3ae3e6f7f6f8bc71d5fe8ce2033464
dls0430 <dls0430@naver.com>
2023-08-03 14:44:14 +0900

    [23.08.03.Thu]-Pro/Lv0/Java/문자 리스트를 문자열로 변환하기

---
+++ LvZero/LvZero012.java
@@ -0,0 +1,13 @@
+package LvZero;
+
+public class LvZero012 {
+    public String solution(String[] arr) {
+        String answer = "";
+
+        for (String x : arr){
+            System.out.print("\"" + x + "\"");
+        }
+
+        return answer;
+    }
+}
